2147e81b58cb7234ac1b14692b22ce9a099f54fd
Víctor Faria <vitilevi@hotmail.com>
2021-04-06 19:03:03 -0300

    Feito ex 1 pt2

--- exercises/bloco_5/dia_2/script.js
+++ exercises/bloco_5/dia_2/script.js
@@ -49,9 +49,10 @@ divMainContent.appendChild(thirdH);
 
 
 // Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
-// Adicione a classe title na tag h1 criada;
-// Adicione a classe description nas 3 tags h3 criadas;
-// Remova o div criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
-// Centralize o div criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto do div ;
-// Troque a cor de fundo do elemento pai da div criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
-// Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+//1 Adicione a classe title na tag h1 criada;
+hBody.className = 'title';
+//2 Adicione a classe description nas 3 tags h3 criadas;
+//3 Remova o div criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+//4 Centralize o div criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto do div ;
+//5 Troque a cor de fundo do elemento pai da div criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+//6 Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
