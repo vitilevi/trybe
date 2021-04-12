036d148e2682687c61f5e847c7d9273e3023f0ed
Víctor Faria <vitilevi@hotmail.com>
2021-04-12 01:15:48 -0300

    Corrigido exercicio dia 5.4

--- exercises/bloco_5/dia_4/lista-de-exercicios/script.js
+++ exercises/bloco_5/dia_4/lista-de-exercicios/script.js
@@ -6,29 +6,58 @@ const fontSizeMenu = document.getElementsByClassName('button-font-size');
 const lineHeightMenu = document.getElementsByClassName('button-line-height');
 const fontFamilyMenu = document.getElementsByClassName('button-font-family');
 
+function checkForLocalStorage() {
+  const backgroundColorStorage = localStorage.getItem('backgroundColor');
+  const colorStorage = localStorage.getItem('color');
+  const fontSizeStorage = localStorage.getItem('fontSize');
+  const lineHeightStorage = localStorage.getItem('lineHeight');
+  const fontFamilyStorage = localStorage.getItem('fontFamily');
+
+  if (backgroundColorStorage) {
+    pText.style.backgroundColor = backgroundColorStorage;
+  }
+  if (colorStorage) {
+    pText.style.color = colorStorage;
+  }
+  if (fontSizeStorage != undefined) {
+    pText.style.fontSize = fontSizeStorage;
+  }
+  if (lineHeightStorage != undefined) {
+    pText.style.lineHeight = lineHeightStorage;
+  }
+  if (fontFamilyStorage != undefined) {
+    pText.style.fontFamily = fontFamilyStorage;
+  }
+}
+
 function changeBackground(event) {
   const color = event.target.innerText;
   pText.style.backgroundColor = color;
+  localStorage.setItem('backgroundColor', color);
 }
 
 function changeColor(event) {
   const color = event.target.innerText;
   pText.style.color = color;
+  localStorage.setItem('color', `${color}`);
 }
 
 function changeFontSize(event) {
   const size = event.target.innerText;
   pText.style.fontSize = size;
+  localStorage.setItem('fontSize', `${size}`);
 }
 
 function changeLineHeight(event) {
   const size = event.target.innerText;
   pText.style.lineHeight = size;
+  localStorage.setItem('lineHeight', `${size}`);
 }
 
 function changeFontFamily(event) {
   const fontChoice = event.target.innerText;
   pText.style.fontFamily = fontChoice;
+  localStorage.setItem('fontFamily', `${fontChoice}`);
 }
 
 function eventListeners() {
@@ -51,6 +80,7 @@ function eventListeners() {
 
 function initialize() {
   eventListeners();
+  checkForLocalStorage();
 }
 
 window.onload = initialize;
