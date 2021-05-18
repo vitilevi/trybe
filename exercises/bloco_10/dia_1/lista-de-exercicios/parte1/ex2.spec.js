9ce54091ecc52b82297777e14a7c23a107d68dbe
Víctor Faria <vitilevi@hotmail.com>
2021-05-18 19:15:44 -0300

    Feito até ex3

--- exercises/bloco_10/dia_1/lista-de-exercicios/parte1/ex2.spec.js
+++ exercises/bloco_10/dia_1/lista-de-exercicios/parte1/ex2.spec.js
@@ -1,19 +1,21 @@
+const { expect } = require('@jest/globals');
 const myRemove = require('./ex2');
 
 describe('Teste a função myRemove', () => {
   it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
-    
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
 
   it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
-
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
 
   it('Verifique se o array passado por parâmetro não sofreu alterações', () => {
-
+    const array = [1, 2, 3, 4]
+    expect(myRemove(array, 3)).toEqual(array);
   });
 
   it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
-
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 })
