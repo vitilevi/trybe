3b2c1e8ced3404b08dfed0ff4994fbd71e820908
Víctor Faria <vitilevi@hotmail.com>
2021-05-18 17:31:28 -0300

    Feito ex1

---
+++ exercises/bloco_10/dia_1/lista-de-exercicios/parte1/ex2.spec.js
@@ -0,0 +1,19 @@
+const myRemove = require('./ex2');
+
+describe('Teste a função myRemove', () => {
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    
+  });
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+
+  });
+
+  it('Verifique se o array passado por parâmetro não sofreu alterações', () => {
+
+  });
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+
+  });
+})
