56cd70e228dbd502cdac088b650278d4030157e2
Víctor Faria <vitilevi@hotmail.com>
2021-05-19 16:12:07 -0300

    Corrigio teste do ex1

--- exercises/bloco_10/dia_2/lista-de-exercicios/ex1.spec.js
+++ exercises/bloco_10/dia_2/lista-de-exercicios/ex1.spec.js
@@ -4,8 +4,11 @@ const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 };
 
-test('Testa se o callback retorna o experado', () => {
-  uppercase('salve salve', (res) => {
-    expect(res).toEqual('SALVE SALVE');
-  });
+test('Testa se o callback retorna o experado', (done) => {
+  setTimeout(() => {
+    uppercase('salve salve', (res) => {
+      expect(res).toEqual('SALVE SALVE');
+      done();
+    });
+  }, 200);
 });
