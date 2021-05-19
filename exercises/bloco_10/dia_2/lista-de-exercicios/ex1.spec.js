f6bc73ba9743ebee4cc569dd4d2bdabff4aee1ba
Víctor Faria <vitilevi@hotmail.com>
2021-05-19 15:30:30 -0300

    Feito ex1

--- exercises/bloco_10/dia_2/lista-de-exercicios/ex1.spec.js
+++ exercises/bloco_10/dia_2/lista-de-exercicios/ex1.spec.js
@@ -1,9 +1,11 @@
+const { test } = require("@jest/globals");
+
 const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 };
 
-test('Testa se o callback retorna o experado', (done) => {
+test('Testa se o callback retorna o experado', () => {
   uppercase('salve salve', (res) => {
     expect(res).toEqual('SALVE SALVE');
-  })
-})
+  });
+});
