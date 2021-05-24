ed29ec8573a1f9450a347fea9dff853a44b5b664
Víctor Faria <vitilevi@hotmail.com>
2021-05-23 22:33:39 -0300

    Feito ex1 da lista

---
+++ exercises/bloco_10/dia_3/Conteudo-2/math-spyOn.spec.js
@@ -0,0 +1,13 @@
+const math = require('../Conteudo-1/math');
+
+test("#somar", () => {
+  // testando se a função foi chamada, quantas vezes foi chamada, quais parâmetros foram usados e qual seu retorno
+  const mockSomar = jest.spyOn(math, "somar");
+
+  mockSomar(1, 2);
+  expect(mockSomar).toHaveBeenCalled();
+  expect(mockSomar).toHaveBeenCalledTimes(1);
+  expect(mockSomar).toHaveBeenCalledWith(1, 2);
+  expect(mockSomar(1, 2)).resolves.toBe(3);
+  // expect.assertions(4);
+});
