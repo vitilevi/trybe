e4ab77308f601a99d75897f133ab1320c8beb30a
Víctor Faria <vitilevi@hotmail.com>
2021-05-24 18:29:14 -0300

    Feito exercicios do conteudo

---
+++ exercises/bloco_11/dia_1/Conteudo-2/meu-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import ReactComponent from './component';
+
+function App() {
+  return <ReactComponent />;
+}
+
+export default App;
---
+++ exercises/bloco_11/dia_1/Conteudo-2/meu-app/src/component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ReactComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Víctor Faria
+        </h1>
+        <p>
+          Olá, me chamo Víctor e tenho 24 anos.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default ReactComponent;
