d8b6d13fc91baaf1f0675899591a93fb0a48781c
Víctor Faria <vitilevi@hotmail.com>
2021-05-23 23:00:56 -0300

    Feito ex4

--- exercises/bloco_10/dia_3/lista-de-exercicios/ex1-2-3-4-5.spec.js
+++ exercises/bloco_10/dia_3/lista-de-exercicios/ex1-2-3-4-5.spec.js
@@ -24,3 +24,22 @@ test('3 - Testa uma nova implementação que receba três parâmetros e retorne 
   expect(randomize(2)).toEqual(4);
 });
 
+test('4 - crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.', () => {
+
+  let toUpCase = (str) => str.toUpperCase();
+  let firstLetter = (str) => str.split('')[0];
+  let concactStr = (str1, str2) => str1.concat(str2);
+
+  expect(toUpCase('Teste testando')).toEqual('TESTE TESTANDO');
+  expect(firstLetter('Teste testando')).toEqual('T');
+  expect(concactStr('Teste', 'testando')).toEqual('Testetestando');
+  
+
+  toUpCase = jest.fn().mockImplementation((str) => str.toLowerCase());
+  firstLetter = jest.fn().mockImplementation((str) => str.split('')[str.length - 1]);
+  concactStr = jest.fn().mockImplementation((str1, str2, str3) => str1.concat(str2).concat(str3));
+
+  expect(toUpCase('Teste testando')).toEqual('teste testando');
+  expect(firstLetter('Teste testando')).toEqual('o');
+  expect(concactStr('Teste', 'testando', '1')).toEqual('Testetestando1');
+});
