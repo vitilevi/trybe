040d96736dcf2540d75bb73304b9c02c41d15710
Víctor Faria <vitilevi@hotmail.com>
2021-05-23 22:48:15 -0300

    Feito ex3

--- exercises/bloco_10/dia_3/lista-de-exercicios/ex1-2-3-4-5.spec.js
+++ exercises/bloco_10/dia_3/lista-de-exercicios/ex1-2-3-4-5.spec.js
@@ -1,6 +1,6 @@
 const randomize = () => Math.round(Math.random() * 100);
 
-test('Testa função randomize ', () => {
+test('1 - Testa função randomize ', () => {
   const randomize = jest.fn().mockReturnValue(10);
 
   randomize();
@@ -8,11 +8,19 @@ test('Testa função randomize ', () => {
   expect(randomize()).toEqual(10);
 });
 
-test('Testa se a função randomize divide dois valores', () => {
+test('2 - Testa se a função randomize divide dois valores', () => {
   const randomize = jest.fn().mockImplementationOnce((a, b) => a / b);
 
   expect(randomize(10, 5)).toEqual(2);
   expect(randomize).toHaveBeenCalled();
   expect(randomize(10, 5)).not.toEqual(2);
+});
+
+test('3 - Testa uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro', () => {
+  const randomize = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+  expect(randomize(1, 2, 3)).toEqual(6);
+  randomize.mockReset().mockImplementation((a) => a * 2);
+  expect(randomize(2)).toEqual(4);
 });
 
