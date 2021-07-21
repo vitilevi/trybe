7f913a7578fd1f8255511f33aeb50c7cf206ed4f
Víctor Faria <vitilevi@hotmail.com>
2021-07-21 14:01:22 -0300

    adicionado exercicio

---
+++ bloco_16/dia_5/Conteudo-1/my-app/src/App.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { render, cleanup } from '@testing-library/react';
+import App from './App';
+import { createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('the page should has a button and a text 0', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+  });
+
+  test('testa como clickReducer começando em 10', () => {
+    const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 10 } } });
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('10')).toBeInTheDocument();
+  });
+});
