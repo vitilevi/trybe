1637cac3a0106af889cfd4fa7bc9c9fabab8c0b2
Víctor Faria <vitilevi@hotmail.com>
2021-08-18 15:44:23 -0300

    Adiciona queries do dia

---
+++ bloco_20/dia_2/Exercícios do conteúdo.sql
@@ -0,0 +1,31 @@
+select 'Víctor';
+select 'Víctor' as nome, 'faria' as sobrenome, 'Guaratinguetá' as 'cidade natal', 25 as idade;
+select 13 * 8;
+select now() as 'Data atual';
+
+use sakila;
+SELECT CONCAT (first_name, last_name) FROM actor;
+SELECT CONCAT (first_name, ' ', last_name) FROM actor;
+SELECT CONCAT (first_name, ' ', last_name) AS 'Nome Completo' FROM actor;
+
+SELECT CONCAT (title, ' ', release_year) as 'Lançamento do filme' from film;
+SELECT CONCAT (title, ' ', rating) as 'Classificação' from film;
+SELECT CONCAT (address, ' ', district) as 'Endereço' from address;
+
+CREATE DATABASE `Escola`;
+CREATE TABLE IF NOT EXISTS Escola.Alunos (
+    `Nome` VARCHAR(7) CHARACTER SET utf8,
+    `Idade` INT
+);
+INSERT INTO Escola.Alunos VALUES
+    ('Rafael', 25),
+    ('Amanda', 30),
+    ('Roberto', 45),
+    ('Carol', 19),
+    ('Amanda', 25);
+    
+USE Escola;
+SELECT Nome, Idade FROM Alunos; -- 5rows 
+SELECT DISTINCT Nome FROM Alunos; -- 4rows
+SELECT DISTINCT Idade FROM Alunos; -- 4rows
+
