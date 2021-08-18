e50dce2f29ab2b82e890b3d37fd7acbc550b92e0
Víctor Faria <vitilevi@hotmail.com>
2021-08-18 18:54:19 -0300

    Adiciona exercicios do dia

--- bloco_20/dia_2/Exercícios do conteúdo.sql
+++ bloco_20/dia_2/Exercícios do conteúdo.sql
@@ -3,14 +3,17 @@ select 'Víctor' as nome, 'faria' as sobrenome, 'Guaratinguetá' as 'cidade nata
 select 13 * 8;
 select now() as 'Data atual';
 
+
 use sakila;
 SELECT CONCAT (first_name, last_name) FROM actor;
 SELECT CONCAT (first_name, ' ', last_name) FROM actor;
 SELECT CONCAT (first_name, ' ', last_name) AS 'Nome Completo' FROM actor;
 
+
 SELECT CONCAT (title, ' ', release_year) as 'Lançamento do filme' from film;
 SELECT CONCAT (title, ' ', rating) as 'Classificação' from film;
 SELECT CONCAT (address, ' ', district) as 'Endereço' from address;
+
 
 CREATE DATABASE `Escola`;
 CREATE TABLE IF NOT EXISTS Escola.Alunos (
@@ -23,9 +26,22 @@ INSERT INTO Escola.Alunos VALUES
     ('Roberto', 45),
     ('Carol', 19),
     ('Amanda', 25);
-    
+
 USE Escola;
 SELECT Nome, Idade FROM Alunos; -- 5rows 
 SELECT DISTINCT Nome FROM Alunos; -- 4rows
 SELECT DISTINCT Idade FROM Alunos; -- 4rows
 
+
+USE sakila;
+SELECT COUNT(password) FROM staff; -- 1
+SELECT COUNT(staff_id) FROM staff; -- 2
+SELECT COUNT(email) FROM staff; -- 1
+
+
+SELECT COUNT(*) FROM rental;
+SELECT * FROM rental;
+SELECT * FROM rental LIMIT 10;
+
+
+SELECT * FROM actor LIMIT 10 OFFSET 4;
