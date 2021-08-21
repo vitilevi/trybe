5f4fd6de0f4c4d16b9bbb77c9881424a9d1e4595
Víctor Faria <vitilevi@hotmail.com>
2021-08-21 13:01:33 -0300

    Feito exercicios de delete

--- bloco_20/dia_4/Exercicios-do-conteudo.sql
+++ bloco_20/dia_4/Exercicios-do-conteudo.sql
@@ -29,10 +29,56 @@ USE sakila;
 UPDATE actor
 SET first_name = 'JULES'
 WHERE first_name = 'JULIA';
--- 2) Foi exigido que a categoria "Sci-Fi" seja alterada para "Science Fiction".
 
+-- 2) Foi exigido que a categoria "Sci-Fi" seja alterada para "Science Fiction".
+UPDATE category
+SET name = 'Science Fiction'
+WHERE name = 'Sci-Fi';
 
 -- 3) Atualize o valor do aluguel para $25 de todos os filmes com duração maior que 100 minutos e que possuem a classificações "G" , "PG" ou "PG-13" e um custo de substituição maior que $20.
-
+UPDATE film
+SET rental_rate = 25
+WHERE replacement_cost > 20
+AND length > 100
+AND rating in ('G', 'PG', 'PG-13');
 
 -- 4) Foi determinado pelo setor financeiro que haverá um reajuste em todos os preços dos filmes, com base em sua duração. Para todos os filmes com duração entre 0 e 100, o valor do aluguel passará a ser $10,00, e o aluguel dos filmes com duração acima de 100 passará a ser de $20,00.
+UPDATE film
+SET rental_rate = (
+	CASE
+		WHEN length BETWEEN 1 AND 100 THEN 10
+		WHEN length > 100 THEN 20
+		ELSE rental_rate
+	END
+);
+
+
+
+-- 1) Exclua do banco de dados o ator com o nome de "KARL".
+DELETE FROM film_actor
+WHERE actor_id = 12;
+
+DELETE FROM actor
+WHERE first_name = 'KARL';
+
+-- 2) Exclua do banco de dados os atores com o nome de "MATTHEW".
+SELECT * FROM actor WHERE first_name = 'MATTHEW';
+DELETE FROM film_actor
+WHERE actor_id in (8, 103, 181);
+
+DELETE FROM actor
+WHERE first_name = 'MATTHEw';
+
+-- 3) Exclua da tabela film_text todos os registros que possuem a palavra "saga" em suas descrições.
+DELETE FROM film_text
+WHERE description
+LIKE '%saga%';
+
+-- 4) Apague da maneira mais performática possível todos os registros das tabelas film_actor e film_category .
+TRUNCATE film_actor;
+TRUNCATE film_category;
+
+-- 5) Inspecione todas as tabelas do banco de dados sakila e analise quais restrições ON DELETE foram impostas em cada uma. Use o Table Inspector para fazer isso (aba DDL).
+
+-- 6) Exclua o banco de dados e o recrie (use as instruções no início desta aula).
+TRUNCATE sakila;
